a08fb94c9130ab853a8da53190fdb15605353bda
Aly <el7amrawy@outlook.com>
2022-12-14 01:01:28 +0200

    1st

---
+++ Rock Paper Scissors/game.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+
+int genRandNum(int offset, int range);
+string ro_pa_sc(int n);
+void clearScreen(int n);
+void userWon(int userScore,int compScore);
+void compWon(int userScore,int compScore);
+
+int main(){
+
+    int userInp,userScore=0,compScore=0;
+
+    while (1)
+    {  
+        int comp=genRandNum(1,3);
+        cout<<"\n1) Rock\n2) Paper\n3) Scissors\n\n0) exit\n\nChoose: ";
+        cin>>userInp;
+
+        if (userInp==0)
+            return 0;
+
+        clearScreen(4);
+        if(userInp>3||userInp<1){
+            cout<<"\n** input is out of range **\n\n";
+        } else{
+            cout<<"\nyou chose: "<<ro_pa_sc(userInp)<<"\n"<<"computer chose: "<<ro_pa_sc(comp)<<"\n";
+            if(userInp==comp){
+                cout<<"\n  *** tie ***\n\n";
+                cout<<"your score: "<<userScore<<"\t"<<"computer score: "<<compScore<<"\n";
+            }
+            else if(userInp<comp && comp-userInp==1){
+                compScore++;
+                compWon(userScore,compScore);
+            }
+            else if(userInp>comp && userInp-comp==1){
+                userScore++;
+                userWon(userScore,compScore);
+            } else{
+                if(userInp==1){
+                    userScore++;
+                    userWon(userScore,compScore);
+                }else{
+                    compScore++;
+                    compWon(userScore,compScore);
+                }
+            }
+        }
+    }
+}
+
+int genRandNum(int offset, int range){
+    srand((unsigned) time(NULL));
+    int random=(rand()%range)+offset;
+    return random;
+}
+
+string ro_pa_sc(int n){
+    if( n==1 ){
+        return "Rock";
+    } else if(n==2){
+        return "Paper";
+    }else{
+        return "Scissors";
+    }
+}
+
+void clearScreen(int n){
+    for(int i=0;i<n;i++){
+        cout<<"\n\n";
+    }
+}
+
+void userWon(int userScore,int compScore){
+    cout<<"\n  *** you won ***\n\n";
+    cout<<"your score: "<<userScore<<"\t"<<"computer score: "<<compScore<<"\n\n";
+}
+
+void compWon(int userScore,int compScore){
+    cout<<"\n  *** computer won ***\n\n";
+    cout<<"your score: "<<userScore<<"\t"<<"computer score: "<<compScore<<"\n\n";
+}
