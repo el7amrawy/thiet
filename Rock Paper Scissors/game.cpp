c0050c33c0111c4bd99f444bc235facd24b7a125
Aly <el7amrawy@outlook.com>
2022-12-20 22:40:11 +0200

    use class to encapsulate

--- Rock Paper Scissors/game.cpp
+++ Rock Paper Scissors/game.cpp
@@ -2,58 +2,13 @@
 
 using namespace std;
 
-int genRandNum(int offset, int range);
-string ro_pa_sc(int n);
-void clearScreen(int n);
-void userWon(int* userScore,int compScore);
-void compWon(int userScore,int* compScore);
-
-int main(){
-
-    int userInp,userScore=0,compScore=0;
-
-    while (1)
-    {  
-        int comp=genRandNum(1,3);
-        cout<<"\n1) Rock\n2) Paper\n3) Scissors\n\n0) exit\n\nChoose: ";
-        cin>>userInp;
-
-        if (userInp==0)
-            return 0;
-
-        clearScreen(4);
-        if(userInp>3||userInp<1){
-            cout<<"\n** input is out of range **\n\n";
-        } else{
-            cout<<"\nyou chose: "<<ro_pa_sc(userInp)<<"\n"<<"computer chose: "<<ro_pa_sc(comp)<<"\n";
-            if(userInp==comp){
-                cout<<"\n  *** tie ***\n\n";
-                cout<<"your score: "<<userScore<<"\t"<<"computer score: "<<compScore<<"\n";
-            }
-            else if(userInp<comp && comp-userInp==1){
-                compWon(userScore,&compScore);
-            }
-            else if(userInp>comp && userInp-comp==1){
-                userWon(&userScore,compScore);
-            } else{
-                if(userInp==1){
-                    userWon(&userScore,compScore);
-                }else{
-                    compWon(userScore,&compScore);
-                }
-            }
-        }
-    }
-}
-
-int genRandNum(int offset, int range){
-    srand((unsigned) time(NULL));
-    int random=(rand()%range)+offset;
-    return random;
-}
-
-string ro_pa_sc(int n){
-    switch (n){
+class Game
+{
+public:
+    string ro_pa_sc(int n)
+    {
+        switch (n)
+        {
         case 1:
             return "Rock";
             break;
@@ -62,23 +17,92 @@ string ro_pa_sc(int n){
             break;
         default:
             return "Scissors";
+        }
+    }
+
+    void userWon(int *userScore, int compScore)
+    {
+        *userScore += 1;
+        cout << "\n  *** you won ***\n\n";
+        cout << "your score: " << *userScore << "\t"
+             << "computer score: " << compScore << "\n\n";
+    }
+
+    void compWon(int userScore, int *compScore)
+    {
+        *compScore += 1;
+        cout << "\n  *** computer won ***\n\n";
+        cout << "your score: " << userScore << "\t"
+             << "computer score: " << *compScore << "\n\n";
+    }
+};
+
+int genRandNum(int offset, int range);
+void clearScreen(int n);
+
+int main()
+{
+    Game g;
+    int userInp, userScore = 0, compScore = 0;
+
+    while (1)
+    {
+        int comp = genRandNum(1, 3);
+        cout << "\n1) Rock\n2) Paper\n3) Scissors\n\n0) exit\n\nChoose: ";
+        cin >> userInp;
+
+        if (userInp == 0)
+            return 0;
+
+        clearScreen(4);
+        if (userInp > 3 || userInp < 1)
+        {
+            cout << "\n** input is out of range **\n\n";
+        }
+        else
+        {
+            cout << "\nyou chose: " << g.ro_pa_sc(userInp) << "\n"
+                 << "computer chose: " << g.ro_pa_sc(comp) << "\n";
+            if (userInp == comp)
+            {
+                cout << "\n  *** tie ***\n\n";
+                cout << "your score: " << userScore << "\t"
+                     << "computer score: " << compScore << "\n";
+            }
+            else if (userInp < comp && comp - userInp == 1)
+            {
+                g.compWon(userScore, &compScore);
+            }
+            else if (userInp > comp && userInp - comp == 1)
+            {
+                g.userWon(&userScore, compScore);
+            }
+            else
+            {
+                if (userInp == 1)
+                {
+                    g.userWon(&userScore, compScore);
+                }
+                else
+                {
+                    g.compWon(userScore, &compScore);
+                }
+            }
+        }
     }
 }
 
-void clearScreen(int n){
-    for(int i=0;i<n;i++){
-        cout<<"\n\n";
+int genRandNum(int offset, int range)
+{
+    srand((unsigned)time(NULL));
+    int random = (rand() % range) + offset;
+    return random;
+}
+
+void clearScreen(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\n\n";
     }
-}
-
-void userWon(int* userScore,int compScore){
-    *userScore+=1;
-    cout<<"\n  *** you won ***\n\n";
-    cout<<"your score: "<<*userScore<<"\t"<<"computer score: "<<compScore<<"\n\n";
-}
-
-void compWon(int userScore,int* compScore){
-    *compScore+=1;
-    cout<<"\n  *** computer won ***\n\n";
-    cout<<"your score: "<<userScore<<"\t"<<"computer score: "<<*compScore<<"\n\n";
 }
