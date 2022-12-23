cfc2ea73f2782f667d7e5acb2f2738c7ddef795e
Aly <el7amrawy@outlook.com>
2022-12-23 20:59:03 +0200

    pass by reference

--- swap.cpp
+++ swap.cpp
@@ -2,23 +2,34 @@
 
 using namespace std;
 
-void swap(int* x,int* y);
-void increament(int* x);
+void swap(int *x, int *y);
+void increament(int *x);
 
-int main(){
-    int x=1,y=2;
-    // swap(&x,&y);
-    increament(&x);
-    cout<<x;
+int main()
+{
+    int x = 1, y = 2;
+    swap(x, y);
+    // increament(&x);
+    cout << x << endl
+         << y;
 }
 
-void swap(int* x,int* y){
-    int z=*x;
-    *x=*y;
-    *y=z;
+void swap(int *x, int *y)
+{
+    int z = *x;
+    *x = *y;
+    *y = z;
 }
 
-void increament(int* x){
-    *x+=1;
+void increament(int *x)
+{
+    *x += 1;
     // cout<<*x<<endl;
+}
+
+void swap2(int &x, int &y)
+{
+    int z = x;
+    x = y;
+    y = z;
 }
